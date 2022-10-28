8d8d7ea4b0eb507afdee836e3494ad26508686c6
jayho-k <sox3579@gmail.com>
2022-10-28 17:53:34 +0900

    fix: 🐛 [BE] chenge form of getComment

--- backend/src/main/java/com/ssafy/backend/repository/CommentRepository.java
+++ backend/src/main/java/com/ssafy/backend/repository/CommentRepository.java
@@ -1,13 +1,16 @@
 package com.ssafy.backend.repository;
 ;
+import com.ssafy.backend.entity.Board;
 import com.ssafy.backend.entity.Comment;
 import com.ssafy.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
-//    Optional<User> findUserById(Long comment_id);
+
+    List<Comment> findByBoardId(Long id);
 }
